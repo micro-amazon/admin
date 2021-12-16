fb4127883d26a4d5bf17e8cc3c4e09cc938e5955
zwan <2zwan@naver.com>
2021-12-16 14:48:03 +0900

    fix: add /admin controller mapping

--- src/main/java/weaveworksdemos/admin/controllers/AdminController.java
+++ src/main/java/weaveworksdemos/admin/controllers/AdminController.java
@@ -4,10 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import weaveworksdemos.admin.services.AdminService;
 
 @RestController
+@RequestMapping("/admin")
 public class AdminController {
 
     @Autowired
