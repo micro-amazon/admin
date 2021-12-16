bcec7ef6d70445c2b271414078f828d9548c870a
zwan <2zwan@naver.com>
2021-12-16 17:57:41 +0900

    chore: add login log

--- src/main/java/weaveworksdemos/admin/controllers/AdminController.java
+++ src/main/java/weaveworksdemos/admin/controllers/AdminController.java
@@ -1,5 +1,6 @@
 package weaveworksdemos.admin.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import weaveworksdemos.admin.services.AdminService;
 
+@Slf4j
 @RestController
 @RequestMapping("/admin")
 public class AdminController {
@@ -18,10 +20,11 @@ public class AdminController {
     @PostMapping("/login")
     public ResponseEntity login(String username, String password) {
         Boolean isLoginSuccess = adminService.isLoginSuccess(username, password);
-
         if(isLoginSuccess) {
+            log.info("login success");
             return new ResponseEntity(HttpStatus.OK);
         } else {
+            log.info("login fail");
             return new ResponseEntity(HttpStatus.UNAUTHORIZED);
         }
     }
