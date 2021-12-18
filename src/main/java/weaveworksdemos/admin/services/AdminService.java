2dc1a47a191a0469d47ea692a7bb1f57139c3bfd
zwan <2zwan@naver.com>
2021-12-18 12:09:04 +0900

    fix: fix isLoginSuccess using DB

--- src/main/java/weaveworksdemos/admin/services/AdminService.java
+++ src/main/java/weaveworksdemos/admin/services/AdminService.java
@@ -12,9 +12,9 @@ public class AdminService {
     private AdminRepository adminRepository;
 
     public boolean isLoginSuccess(String username, String password) {
-        // temporary workaround
-        return username.equals("admin") && password.equals("password");
-//        return adminRepository.existsByUsernameAndPassword(username, password);
+//         temporary workaround
+//        return username.equals("admin") && password.equals("password");
+        return adminRepository.existsByUsernameAndPassword(username, password);
     }
 
     public Long saveAdmin(String username, String password) {
